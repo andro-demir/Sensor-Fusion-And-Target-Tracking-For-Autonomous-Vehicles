591d5aaa231855f42502753ffff94b70aabd8c08
andac-demir <andacdemir@hotmail.com>
2019-02-28 15:34:06 -0500

    running sensor data association bugless

--- simulator/demo.py
+++ simulator/demo.py
@@ -19,23 +19,29 @@ def createSensorEnvs():
 def main():
     cam_rear, cam_front, radar_rear, radar_front = createSensorEnvs()
     time_frame = [cam_rear.list_time, cam_front.list_time,
-                radar_rear.list_time, radar_rear.list_time]
+                  radar_rear.list_time, radar_rear.list_time]
 
     # Create the time frame from unique entities of the times recorded for each
     # sensor. This will serve as the time scale for the rest of the system.
     time_frame = list(np.unique(np.concatenate(time_frame)))
 
     # Initialize the fusion list:
-    fusionList = []
+    list_object_cam_rear, _ = cam_rear.return_obstacle_list(time_frame[0])
+    list_object_cam_front, _ = cam_front.return_obstacle_list(time_frame[0])
+    list_object_radar_front, _ = radar_rear.return_obstacle_list(time_frame[0])
+    list_object_radar_rear, _ = radar_front.return_obstacle_list(time_frame[0])
+    fusionList = (list_object_cam_front + list_object_cam_rear +
+                  list_object_radar_front + list_object_radar_rear)  
 
     for time in time_frame:
         list_object_cam_rear, _ = cam_rear.return_obstacle_list(time)
         list_object_cam_front, _ = cam_front.return_obstacle_list(time)
         list_object_radar_front, _ = radar_rear.return_obstacle_list(time)
         list_object_radar_rear, _ = radar_front.return_obstacle_list(time)
+        
         # Sensor data association
-        for sensorObjList in (list_object_cam_rear + list_object_cam_front +
-                              list_object_radar_rear + list_object_radar_front):
+        for sensorObjList in ([list_object_cam_front] + [list_object_cam_rear] +
+                              [list_object_radar_front] + [list_object_radar_rear]):
             assc = Association(fusionList, sensorObjList)
             assc.updateExistenceProbability()
             # to get the H matrix call assc.rowInd and assc.colInd at each iter
@@ -43,6 +49,9 @@ def main():
             
             # to update the fusion list:
             fusionList = assc.fusionList
+            for obstacle in fusionList:
+                print("Time: %f, State Vector:" %time)
+                print(obstacle.s_vector)
 
 
 if __name__ == "__main__":
